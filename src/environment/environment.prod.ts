01a39bce3d223afbe989df37a7983462d1a6f90e
GWUY <buigiahuy1506@gmail.com>
2026-03-25 15:43:15 +0700

    change public api

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://internhub-v2.bbtech.io.vn/api',
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://internhub-v2.bbtech.io.vn/api',
 };
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://internhub-v2.bbtech.io.vn/api',
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://internhub-v2.bbtech.io.vn/api',
 };
