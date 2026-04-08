5c79ac036b962dcd2409788be672dcfc9fee2c5f
vominhdat68 <datminhvo0901@gmail.com>
2026-04-08 22:14:53 +0700

    Fix ticket app environment imports

--- src/app/services/attendance-management.service.ts
+++ src/app/services/attendance-management.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environment/environment';
 
 export interface AttendanceFilterResponse {
   attendanceDate: string;
--- src/app/services/hrm-user-management.service.ts
+++ src/app/services/hrm-user-management.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environment/environment';
 
 // Backend FilterRequest structure
 export interface FilterRequest {
