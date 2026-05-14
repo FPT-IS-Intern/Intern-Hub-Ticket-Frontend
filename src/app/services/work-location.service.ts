6c46881fdc29e0f76d44c54daaa8435889f2366a
vominhdat68 <datminhvo0901@gmail.com>
2026-05-14 23:57:41 +0700

    fix(api): update branches endpoint to call ticket service instead of bo-portal

--- src/app/services/work-location.service.ts
+++ src/app/services/work-location.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ResponseApi } from '../models/ticket.model';
 import { environment } from '../../environment/environment';
 
-export interface BoPortalBranchResponse {
+export interface TicketBranchResponse {
   id: string;
   name: string;
   description?: string | null;
@@ -16,24 +16,25 @@ export interface BoPortalBranchResponse {
 })
 export class WorkLocationService {
   private readonly hrmBaseUrl = `${environment.apiUrl}/hrm/work-locations`;
-  private readonly boPortalBaseUrl = `${environment.apiUrl}/bo-portal/internal`;
+  private readonly ticketBaseUrl = `${environment.apiUrl}/ticket`;
 
   constructor(private http: HttpClient) {}
 
   /**
    * Get all work locations
-  * GET /hrm/work-locations/all-location
-  * Returns a list of location name strings
-  */
+   * GET /hrm/work-locations/all-location
+   * Returns a list of location name strings
+   */
   getAllLocations(): Observable<ResponseApi<string[]>> {
     return this.http.get<ResponseApi<string[]>>(`${this.hrmBaseUrl}/all-location`);
   }
 
   /**
-   * Get all branches from BO Portal
-   * GET /bo-portal/internal/branches
+   * Get all branches from Ticket Service
+   * GET /ticket/branches
    */
-  getAllBranches(): Observable<ResponseApi<BoPortalBranchResponse[]>> {
-    return this.http.get<ResponseApi<BoPortalBranchResponse[]>>(`${this.boPortalBaseUrl}/branches`);
+  getAllBranches(): Observable<ResponseApi<TicketBranchResponse[]>> {
+    return this.http.get<ResponseApi<TicketBranchResponse[]>>(`${this.ticketBaseUrl}/branches`);
   }
 }
+
